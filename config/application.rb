77631dafaaf7ac8a667c8ee937c18e5e0127d438
Josh Hunt <30326121+JHunt16@users.noreply.github.com>
2023-09-07 08:57:19 +0100

    Set the timezone to Europe/London.

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module LaaCrimeApplyMockApi
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "London"
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
