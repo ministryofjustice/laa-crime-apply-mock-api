ac40ff538e5518577ce0b26c7c8e44901bb3c988
Gangadhar.Nitta <gangadhar.nitta@digital.justice.gov.uk>
2023-09-07 12:51:11 +0100

    [LASB-2312] Removed unused route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get 'api/message', to: 'application#message'
-  get 'api/v1/maat/applications', to: 'application#maatapplication'
 
   mount Datastore::Root => '/'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
