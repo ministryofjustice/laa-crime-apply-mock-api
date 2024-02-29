4ac10e31404d0690dadf2264f434a3e26de07a0f
Gangadhar.Nitta <gangadhar.nitta@digital.justice.gov.uk>
2024-02-29 10:01:47 +0000

    [LASB-3059] updating schema version 40

--- app/api/datastore/entities/v1/base_application_entity.rb
+++ app/api/datastore/entities/v1/base_application_entity.rb
@@ -7,52 +7,20 @@ module Datastore
         expose :reference
         expose :application_type
         expose :submitted_at
-        expose :date_stamp
-
-        expose :ioj_passport
-        expose :means_passport
-
-        expose :provider_details
-        expose :client_details
-        expose :case_details
-        expose :interests_of_justice
+        expose :status
+        expose :reviewed_at
+        expose :parent_id
+        expose :created_at
+        expose :work_stream
 
         private
 
-        def id
-          submitted_value('id')
-        end
-
-        def schema_version
-          submitted_value('schema_version')
-        end
-
-        def reference
-          submitted_value('reference')
+        def additional_information
+          submitted_value('additional_information')
         end
 
         def application_type
           submitted_value('application_type')
-        end
-
-        def date_stamp
-          submitted_value('date_stamp')
-        end
-
-        def ioj_passport
-          submitted_value('ioj_passport')
-        end
-
-        def means_passport
-          submitted_value('means_passport')
-        end
-
-        def provider_details
-          submitted_value('provider_details')
-        end
-
-        def client_details
-          submitted_value('client_details')
         end
 
         def case_details
@@ -61,12 +29,63 @@ module Datastore
           case_details
         end
 
+        def client_details
+          submitted_value('client_details')
+        end
+
+        def date_stamp
+          submitted_value('date_stamp')
+        end
+
+        # created_at is the date when the application was started on
+        # crime apply and therefore we take the value from the application
+        # json rather than the table
+        def created_at
+          submitted_value('created_at')
+        end
+
+        def id
+          submitted_value('id')
+        end
+
+        def ioj_passport
+          submitted_value('ioj_passport')
+        end
+
         def interests_of_justice
           submitted_value('interests_of_justice')
         end
 
+        def means_details
+          submitted_value('means_details') || {}
+        end
+
+        def means_passport
+          submitted_value('means_passport')
+        end
+
+        def parent_id
+          submitted_value('parent_id')
+        end
+
+        def provider_details
+          submitted_value('provider_details')
+        end
+
+        def reference
+          submitted_value('reference')
+        end
+
+        def schema_version
+          submitted_value('schema_version')
+        end
+
         def submitted_value(name)
           object.submitted_application&.dig(name)
+        end
+
+        def supporting_evidence
+          submitted_value('supporting_evidence') || []
         end
       end
     end
--- app/api/datastore/entities/v1/maat/application.rb
+++ app/api/datastore/entities/v1/maat/application.rb
@@ -3,21 +3,60 @@ module Datastore
     module V1
       module MAAT
         class Application < BaseApplicationEntity
-          unexpose :ioj_passport,
-                   :interests_of_justice
+          unexpose(
+            :created_at,
+            :parent_id,
+            :work_stream,
+            :reviewed_at,
+            :status
+          )
 
+          expose :case_details
+          expose :client_details
+          expose :date_stamp
+          expose :ioj_bypass
+          expose :means_passport
+          expose :provider_details
           expose :submitted_at, as: :declaration_signed_at
-          expose :ioj_bypass, proc: ->(_) { interests_of_justice.empty? }
+          expose :means_details do
+            expose :income_details
+            expose :outgoings_details
+          end
 
           private
 
           def case_details
-            super.except(
-              'offences',
-              'codefendants',
-              'is_first_court_hearing',
-              'first_court_hearing_name'
-            )
+            super.slice(*%w[
+                          urn
+                          case_type
+                          appeal_maat_id
+                          appeal_lodged_date
+                          appeal_with_changes_details
+                          offence_class
+                          hearing_court_name
+                          hearing_date
+                        ])
+          end
+
+          def income_details
+            means_details.fetch('income_details', nil)&.slice(%w[
+                                                                benefits
+                                                                dependants
+                                                                employment_type
+                                                                employment_details
+                                                                other_income
+                                                              ])
+          end
+
+          def outgoings_details
+            means_details.fetch('outgoings_details', nil)&.slice(%w[
+                                                                   outgoings
+                                                                   housing_payment_type
+                                                                 ])
+          end
+
+          def ioj_bypass
+            interests_of_justice.blank?
           end
         end
       end
